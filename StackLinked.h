fe099935325ffad0ccb8c973cb0c2c0901b4beec
29iA <29congtuanp@gmail.com>
2017-02-23 09:46:49 -0600

    Finished peek & push, building pop

--- StackLinked.h
+++ StackLinked.h
@@ -6,93 +6,94 @@
 template < class T >
 class StackLinked
 {
-   private:
-      NextNode<T>* top;
-      int sze;  // number of items in the stack
+	private:
+		NextNode<T>* top;
+		int sze;  // number of items in the stack
 
-   public:
-      StackLinked();
-      ~StackLinked();
-      bool isEmpty();
-      int size();
-      void popAll();
-      T* pop();
-      void push(T* item);
-      T* peek();
+	public:
+		StackLinked();
+		~StackLinked();
+		bool isEmpty();
+		int size();
+		void popAll();
+		T* pop();
+		void push(T* item);
+		T* peek();
 };
 
 template < class T >
 StackLinked<T>::StackLinked()
 {
-   top = NULL;
-   sze = 0;
+	top = NULL;
+	sze = 0;
 }
 
 template < class T >
 StackLinked<T>::~StackLinked()
 {
-   popAll();
+	popAll();
 }
 
 template < class T >
 bool StackLinked<T>::isEmpty()
 {
-   return sze == 0;
+	return sze == 0;
 }
 
 template < class T >
 int StackLinked<T>::size()
 {
-   return sze;
+	return sze;
 }
 
 template < class T >
 void StackLinked<T>::popAll()
 {
-   //loop over the stack, deleting the nodes
-   //the actual items are not deleted
-   if (sze == 0) return;
+	//loop over the stack, deleting the nodes
+	//the actual items are not deleted
+	if (sze == 0) return;
 
-   NextNode<T>* curr = top;
-   NextNode<T>* prev = NULL;
-   while (curr != NULL)
-   {
-      prev = curr;
-      curr = curr->getNext();
-      prev->setNext(NULL);
-      delete prev;
-   }
+	NextNode<T>* curr = top;
+	NextNode<T>* prev = NULL;
+	while (curr != NULL)
+	{
+		prev = curr;
+		curr = curr->getNext();
+		prev->setNext(NULL);
+		delete prev;
+	}
 }
 
 template < class T >
 T* StackLinked<T>::peek()
 {
-   T* item = NULL;
-   //DO THIS
-
-
-
-
-
+	T* item = NULL;
+	//DO THIS
+	if (!isEmpty()) 
+	{  
+		item = top->getNext();
+	}
+	return item;
 }
 
 template < class T >
 void StackLinked<T>::push(T* item)
 {
-   //DO THIS
-
-
-
-
+	//DO THIS
+	NextNode<T>* node = new NextNode<T>(item);
+	node->setNext(item);
+	top = node;
+	sze++;
+	delete node;
 }
 
 template < class T >
 T* StackLinked<T>::pop()
 {
-   if (sze == 0) return NULL;
+	if (sze == 0) return NULL;
 
-   //DO THIS
-
+	//DO THIS
+	
 
 
 
